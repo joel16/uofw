67109e6144f4420c5e5a48b4715e2fe061931749
Joel16 <joel16@iastate.edu>
2020-07-18 13:42:47 -0400

    Initial libparse_http reverse

---
+++ src/libparse_http/libparse_http.c
@@ -0,0 +1,209 @@
+/* Copyright (C) 2020 The uOFW team
+   See the file COPYING for copying permission.
+*/
+
+#include <common_imp.h>
+
+SCE_MODULE_INFO("SceParseHTTPheader_Library", SCE_MODULE_ATTR_EXCLUSIVE_LOAD | SCE_MODULE_ATTR_EXCLUSIVE_START, 1, 1);
+SCE_SDK_VERSION(SDK_VERSION);
+
+#define SCE_PARSE_HTTP_NOT_FOUND        0x80432025 // lui $s2, 0x8043 -> ori $v0, $s2, 0x2025
+#define SCE_PARSE_HTTP_INVALID_RESPONSE 0x80432060 // lui $a0, 0x8043 -> ori $v0, $a0, 0x2060
+
+// Internal function prototyes
+s32 sub_000003A0(u8 *status_line, u32 line_len, s32 *http_major_ver, s32 *http_minor_ver);
+s32 sub_00000510(u8 *status_line, u32 line_len, s32 *response_code);
+s32 sub_0000061C(u8 *status_line, u32 line_len, u8 **reason_phrase, u32 *phrase_len);
+s32 sub_000006CC(u8 *unk, u8 *status_line, s32 len);
+
+// Subroutine sceParseHttp_8077A433 - Address 0x00000000
+s32 sceParseHttpStatusLine(const u8 *status_line, u32 line_len, u32 *http_major_ver, u32 *http_minor_ver, u32 *response_code, const u8 **reason_phrase, u32 *phrase_len)
+{
+	// References _sceKernelCheckThreadStack (0xD13BDE95)
+	return 0;
+}
+
+// Subroutine sceParseHttp_AD7BFDEF - Address 0x00000118
+s32 sceParseHttpResponseHeader(const u8 *header, u32 header_len, const u8 *field_name, const u8 **field_value, u32 *value_len)
+{
+	return 0;
+}
+
+// Subroutine sub_000003A0 - Address 0x000003A0
+s32 sub_000003A0(u8 *status_line, u32 line_len, s32 *http_major_ver, s32 *http_minor_ver)
+{
+	s32 var0;
+	u8 *var1;
+	*http_major_ver = 0;
+	*http_minor_ver = 0;
+
+	s32 ret = -0x7fbcdfa0;
+
+	if (8 < line_len)
+	{
+		var0 = sub_000006CC(status_line, (u8 *)"HTTP/", 5);
+		ret = -0x7fbcdfa0;
+
+		if (var0 == 0)
+		{
+			ret = -0x7fbcdfa0;
+
+			if ((*(u8 *)((s32)status_line[5] + 1) & 4) != 0)
+			{
+				var1 = status_line + 5;
+				ret = *http_major_ver;
+				var0 = 5;
+
+				while(1)
+				{
+					*http_major_ver = ret * 10 + (s32)*var1 + -0x30;
+					var1 = status_line + var0 + 1;
+
+					if ((*(u8 *)((s32)*var1 + 1) & 4) == 0)
+						break;
+
+					ret = *http_major_ver;
+					var0 = var0 + 1;
+				}
+
+				ret = -0x7fbcdfa0;
+
+				if ((s32)*var1 == 0x2e)
+				{
+					var1 = status_line + var0 + 2;
+					ret = -0x7fbcdfa0;
+
+					if ((*(u8 *)((s32)*var1 + 1) & 4) != 0)
+					{
+						ret = *http_minor_ver;
+						var0 = var0 + 2;
+
+						while(1)
+						{
+							*http_minor_ver = ret * 10 + (s32)*var1 + -0x30;
+							var1 = status_line + var0 + 1;
+
+							if ((*(u8 *)((s32)*var1 + 1) & 4) == 0)
+								break;
+
+							ret = *http_minor_ver;
+							var0 = var0 + 1;
+						}
+
+						ret = -0x7fbcdfa0;
+
+						if ((s32)*var1 == 0x20)
+							ret = var0 + 2;
+					}
+				}
+			}
+		}
+	}
+
+	return ret;
+}
+
+// Subroutine sub_00000510 - Address 0x00000510
+s32 sub_00000510(u8 *status_line, u32 line_len, s32 *response_code)
+{
+	s32 ret = SCE_PARSE_HTTP_INVALID_RESPONSE;
+
+	if (3 < line_len)
+	{
+		ret = SCE_PARSE_HTTP_INVALID_RESPONSE;
+
+		if (((*(u8 *)((s32)*status_line + 1) & 4) != 0) && ((*(u8 *)((s32)status_line[1] + 1) & 4) != 0))
+		{
+			if ((*(u8 *)((s32)status_line[2] + 1) & 4) != 0)
+			{
+				ret = 3;
+				*response_code = (s32)*status_line * 100 + (s32)status_line[1] * 10 + (s32)status_line[2] + -0x14d0;
+			}
+		}
+	}
+
+	return ret;
+}
+
+// Subroutine sub_0000061C - Address 0x0000061C
+s32 sub_0000061C(u8 *status_line, u32 line_len, u8 **reason_phrase, u32 *phrase_len)
+{
+	u8 var0 = 0;
+	u8 *var1 = status_line;
+	u32 var2 = 0;
+	
+	while(1)
+	{
+		if (*var1 == '\n')
+		{
+			if (var2 != 0)
+				var0 = var1[-1] == '\r';
+			
+			if (var0)
+				*phrase_len = var2 - 1;
+			else
+				*phrase_len = var2;
+				
+			*reason_phrase = status_line;
+			return var2 + 1;
+		}
+		
+		var2 = var2 + 1;
+		if (line_len < var2)
+			break;
+			
+		var1 = status_line + var2;
+	}
+	
+	return -0x7fbcdfa0;
+}
+
+// Subroutine sub_000006CC - Address 0x000006CC
+s32 sub_000006CC(u8 *unk, u8 *status_line, s32 len)
+{
+	u8 var0;
+	s32 ret = 0;
+	u8 *var2;
+	u8 var3;
+
+	if (unk == (u8 *)0x0 || status_line == (u8 *)0x0)
+	{
+		if ((unk != status_line) && (ret = -1, unk != (u8 *)0x0))
+			ret = 1;
+	}
+	else
+	{
+		len = len + -1;
+
+		if (-1 < len)
+		{
+			var3 = *unk;
+			var2 = status_line + 1;
+			if (var3 == *status_line)
+			{
+				do
+				{
+					len = len + -1;
+					unk = unk + 1;
+
+					if (var3 == 0)
+						return 0;
+
+					if (len < 0)
+						return 0;
+
+					var0 = *var2;
+					var3 = *unk;
+					var2 = var2 + 1;
+				} while (var3 == var0);
+			}
+
+			if (len < 0)
+				ret = 0;
+			else
+				ret = (s32)*unk - (s32)var2[-1];
+		}
+	}
+
+	return ret;
+}
